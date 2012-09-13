1f7a68d63e264d7f6ab5a0e697991bea8442f33c
Ahmad Amireh <ahmad.amireh@gmail.com>
2012-09-13 09:56:32 +0300

    Spec update: wrote the pending parsing tests, and added 11 others.
    New tests cover bad input (missing or too many arguments),
    bad invocations based on the key type (short vs expanded notations),
    and proper parsing and population of the options regardless of the
    notation used.

--- spec/cliargs_parsing_spec.lua
+++ spec/cliargs_parsing_spec.lua
@@ -16,7 +16,27 @@ local dump = function(t)
   print(" ============= Dump " .. tostring(t) .. " =============")
 end
 
-local cli
+-- fixture
+local function populate_required(cli)
+  cli:add_argument("INPUT", "path to the input file")
+
+  return { ["INPUT"] = nil }
+end
+local function populate_optionals(cli)
+  cli:add_option("-c, --compress=FILTER", "the filter to use for compressing output: gzip, lzma, bzip2, or none", nil, "gzip")
+  cli:add_option("-o FILE", "path to output file", nil, "/dev/stdout")  
+
+  return { c = "gzip", compress = "gzip", o = "/dev/stdout" }
+end
+local function populate_flags(cli)
+  cli:add_flag("-d", "script will run in DEBUG mode")
+  cli:add_flag("-v, --version", "prints the program's version and exits")
+  cli:add_flag("--verbose", "the script output will be very verbose")  
+
+  return { d = false, v = false, version = false, verbose = false }
+end
+
+local cli, defaults, result
 -- start tests
 describe("Testing cliargs library parsing commandlines", function()
 
@@ -30,28 +50,147 @@ describe("Testing cliargs library parsing commandlines", function()
     _TEST = false
   end)
   
+  before_each(function()
+    cli.optional = {}
+    cli.required = {}
+  end)
+
   it("tests no arguments set, nor provided", function()
     arg = nil
     result = cli:parse()
     assert.are.same(result, {})
   end)
   
-  pending("tests only optionals, nothing provided", function()
+  it("tests only optionals, nothing provided", function()
+    arg = nil
+    defaults = populate_optionals(cli)
+    result = cli:parse()
+    assert.are.same(result, defaults)
   end)
   
-  pending("tests only required, all provided", function()
+  it("tests only required, all provided", function()
+    arg = { "some_file" }
+    populate_required(cli)
+    result = cli:parse()
+    assert.are.same(result, { ["INPUT"] = "some_file" })
   end)
   
-  pending("tests only optionals, all provided", function()
+  it("tests only optionals, all provided", function()
+    arg = { "-o", "/dev/cdrom", "--compress=lzma" }
+    populate_optionals(cli)
+    result = cli:parse()
+    assert.are.same(result, { o = "/dev/cdrom", c = "lzma", compress = "lzma" })
   end)
   
-  pending("tests optionals + required, all provided", function()
-  end)
-  
-  pending("tests optionals + required, no optionals and to little required provided, ", function()
+  it("tests optionals + required, all provided", function()
+    arg = { "-o", "/dev/cdrom", "-c", "lzma", "some_file" }
+    populate_required(cli)
+    populate_optionals(cli)
+    result = cli:parse()
+    assert.are.same(result, {
+      o = "/dev/cdrom",
+      c = "lzma", compress = "lzma",
+      ["INPUT"] = "some_file"
+    })
   end)
 
-  pending("tests optionals + required, no optionals and to many required provided, ", function()
+  it("tests optional using -short-key notation", function()
+    arg = { "-c", "lzma" }
+    defaults = populate_optionals(cli)
+    defaults.c = "lzma"
+    defaults.compress = "lzma"
+
+    result = cli:parse()
+    assert.are.same(result, defaults)
+  end)
+
+  it("tests optional using --expanded-key notation", function()
+    arg = { "--compress=lzma" }
+    defaults = populate_optionals(cli)
+    defaults.c = "lzma"
+    defaults.compress = "lzma"
+
+    result = cli:parse()
+
+    assert.are.same(result, defaults)
   end)
   
+  it("tests flag using -short-key notation", function()
+    arg = { "-v" }
+    defaults = populate_flags(cli)
+    defaults.v = true
+    defaults.version = true
+    result = cli:parse()
+    assert.are.same(result, defaults)
+  end)
+
+  it("tests flag using --expanded-key notation", function()
+    arg = { "--version" }
+    defaults = populate_flags(cli)
+    defaults.v = true
+    defaults.version = true
+    result = cli:parse()
+    assert.are.same(result, defaults)
+  end)
+  
+  it("tests sk-only flag", function()
+    arg = { "-d" }
+    defaults = populate_flags(cli)
+    defaults.d = true
+    result = cli:parse()
+    assert.are.same(result, defaults)
+  end)
+    
+  it("tests ek-only flag", function()
+    arg = { "--verbose" }
+    defaults = populate_flags(cli)
+    defaults.verbose = true
+    result = cli:parse()
+    assert.are.same(result, defaults)
+  end)
+  
+  it("tests optionals + required, no optionals and to little required provided, ", function()
+    arg = { }
+    populate_required(cli)
+    populate_optionals(cli)
+    result = cli:parse(true --[[no print]])
+    assert.is.falsy(result)
+  end)
+
+  it("tests optionals + required, no optionals and to many required provided, ", function()
+    arg = { "some_file", "some_other_file" }
+    populate_required(cli)
+    populate_optionals(cli)
+    result = cli:parse(true --[[no print]])
+    assert.is.falsy(result)
+  end)
+  
+  it("tests bad short-key notation, -x=VALUE", function()
+    arg = { "-o=some_file" }
+    populate_optionals(cli)
+    result = cli:parse(true --[[no print]])
+    assert.is.falsy(result)
+  end)
+  
+  it("tests bad --expanded-key notation, --x VALUE", function()
+    arg = { "--compress", "lzma" }
+    populate_optionals(cli)
+    result = cli:parse(true --[[no print]])
+    assert.is.falsy(result)
+  end)  
+
+  it("tests unknown option", function()
+    arg = { "--foo=bar" }
+    populate_optionals(cli)
+    result = cli:parse(true --[[no print]])
+    assert.is.falsy(result)
+  end)
+
+  it("tests unknown flag", function()
+    arg = { "--foo" }
+    populate_optionals(cli)
+    result = cli:parse(true --[[no print]])
+    assert.is.falsy(result)
+  end)
+
 end)
