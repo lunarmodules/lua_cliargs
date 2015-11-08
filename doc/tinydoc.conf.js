57ae11499a070876f0d8eedde46c0e807b6dfcd0
Ahmad Amireh <ahmad@amireh.net>
2015-11-09 00:39:05 +0200

    using the gitbooks plugin

--- doc/tinydoc.conf.js
+++ doc/tinydoc.conf.js
@@ -3,7 +3,6 @@ var path = require('path');
 exports.assetRoot = path.resolve(__dirname, '..');
 exports.outputDir = path.resolve(__dirname, 'compiled');
 exports.layout = 'single-page';
-exports.stylesheet = 'doc/style.less';
 exports.title = 'lua_cliargs';
 exports.scrollSpying = true;
 exports.resizableSidebar = false;
@@ -12,5 +11,7 @@ exports.collapsibleSidebar = true;
 exports.plugins = [
   require('tinydoc-plugin-lua')({
     source: 'src/**/*.lua'
-  })
-]
+  }),
+
+  require('tinydoc-theme-gitbooks')()
+];
